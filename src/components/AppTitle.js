8a07beca0b3e818540b844cfca45a6aa67821470
solahidris <emailsolah@gmail.com>
2023-06-19 02:32:46 +0900

    added vercel

--- src/components/AppTitle.js
+++ src/components/AppTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppTitle = () => {
   return (
-    <div className="px-5 py-5 mx-5 bg-black/50 rounded-lg h-max mb-5 mt-[3rem]">
+    <div className="px-5 py-5 mx-5 bg-black/50 rounded-lg h-max mb-5 mt-[3rem] flex justify-center">
       <p className="text-white text-3xl mx-4 font-mono font-bold text-center">
         Recipe App with Dynamic Search
       </p>
